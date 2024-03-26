ad5533c502bb68b13b4bf02e97c983aab05ab669
jakobadel <jakoba@umich.edu>
2024-03-26 09:12:56 -0400

    Log health

--- backend/parseMta.js
+++ backend/parseMta.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/health') {
+    console.log("Healthy")
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({status: 'healthy'}))
